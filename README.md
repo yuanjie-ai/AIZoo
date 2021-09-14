f4ed7607ce9ac401ba78e322825b20b755bc1c82
yuanjie <yuanjie@xiaomi.com>
2021-09-14 16:26:17 +0800

    init

--- README.md
+++ README.md
@@ -3,12 +3,9 @@
 ---
 
 
-
 ![image](https://img.shields.io/pypi/v/aizoo.svg)
-https://pypi.python.org/pypi/aizoo
 
 ![image](https://img.shields.io/travis/Jie-Yuan/aizoo.svg)
-https://travis-ci.com/Jie-Yuan/aizoo
 
 ![image](https://readthedocs.org/projects/aizoo/badge/?version=latest)
 
--- aizoo/tab/models.py
+++ aizoo/tab/models.py
@@ -165,17 +165,68 @@ class CatBoostRegressor(OOF):
             return clf.predict(X_valid), clf.predict(X_test)
 
 
+class TabNetClassifier(OOF):
+
+    def fit_predict(self, X_train, y_train, X_valid, y_valid, X_test, **kwargs):
+        from pytorch_tabnet import tab_model
+
+        clf = tab_model.TabNetClassifier()
+        if self.params is not None:
+            clf.set_params(**self.params)
+
+        eval_set = [(X_train, y_train), (X_valid, y_valid)]
+        self.clf = clf.fit(
+            X_train, y_train,
+            eval_set=eval_set,
+            eval_metric=None,
+            eval_name=('Train', 'Valid'),
+            max_epochs=100,
+            patience=5,
+        )
+        if hasattr(clf, 'predict_proba'):
+            return clf.predict_proba(X_valid), clf.predict_proba(X_test)
+        else:
+            return clf.predict(X_valid), clf.predict(X_test)
+
+
+class TabNetRegressor(OOF):
+
+    def fit_predict(self, X_train, y_train, X_valid, y_valid, X_test, **kwargs):
+        from pytorch_tabnet import tab_model
+
+        clf = tab_model.TabNetRegressor()
+        if self.params is not None:
+            clf.set_params(**self.params)
+
+        y_train = y_train.reshape(-1, 1)
+        y_valid = y_valid.reshape(-1, 1)  # nn输入的不同
+        eval_set = [(X_train, y_train), (X_valid, y_valid)]
+        self.clf = clf.fit(
+            X_train, y_train,
+            eval_set=eval_set,
+            eval_metric=None,
+            eval_name=('Train', 'Valid'),
+            max_epochs=100,
+            patience=5,
+        )
+        if hasattr(clf, 'predict_proba'):
+            return clf.predict_proba(X_valid).reshape(-1), clf.predict_proba(X_test).reshape(-1)
+        else:
+            return clf.predict(X_valid).reshape(-1), clf.predict(X_test).reshape(-1)  # (1000, 1)
+
+
 if __name__ == '__main__':
     from sklearn.metrics import r2_score, roc_auc_score
     from sklearn.datasets import make_regression, make_classification
 
-    for Model in [CatBoostClassifier, LGBMClassifier, XGBClassifier]:
+    for Model in [TabNetClassifier, CatBoostClassifier, LGBMClassifier, XGBClassifier]:
         X, y = make_classification(n_samples=1000)
         oof = Model()
         oof.run(X, y, feval=roc_auc_score, cv=5)
+        break
 
-    for Model in [CatBoostRegressor, LGBMRegressor, XGBRegressor]:
+    for Model in [TabNetRegressor, CatBoostRegressor, LGBMRegressor, XGBRegressor]:
         X, y = make_regression(n_samples=1000)
         oof = Model()
         oof.run(X, y, feval=r2_score, cv=5)
-
+        break
